f3114cfde7b097d3edc8212f25c555bb00bf594a
mailmindlin <liamttlyrox@gmail.com>
2015-10-09 15:10:37 -0500

    Switched away from apache http utils

--- Server/src/com/mindlin/mobilevr/Main.java
+++ Server/src/com/mindlin/mobilevr/Main.java
@@ -1,10 +1,7 @@
 package com.mindlin.mobilevr;
 
-import com.mindlin.mobilevr.server.HttpServerRunner;
-
 public class Main {
 	public static void main(String...fred) {
-		HttpServerRunner server = new HttpServerRunner();
-		server.run();
+		
 	}
 }
